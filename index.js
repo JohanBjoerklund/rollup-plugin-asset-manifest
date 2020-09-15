f07708c7731b60a717291836ab8868af6d07e0e8
Johan Bjorklund <johan.bjorklund@protonmail.ch>
2020-09-15 09:32:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const getChunkInfo = bundle => Object.values(bundle)[0]
+const getChunkKey = bundle => Object.keys(bundle)[0]
+
+const generate = async (params, options, bundle) => {
+
+  const prefix = options.dir ? `${options.dir}/` : ''
+
+  return `{
+    "${params.manifestKey}": "${prefix}${getChunkKey(bundle)}"
+  }`
+}
+
+export default function(params = { manifestKey = 'bundle' }) {
+  return {
+    name: 'rollup-plugin-asset-manifest',
+    generateBundle: async function(options, bundle) {
+
+      const type = 'asset'
+      const { name } = getChunkInfo(bundle)
+      const source = await generate(params, options, bundle)
+
+      this.emitFile({ type, name, source })
+    }
+  }
+}
